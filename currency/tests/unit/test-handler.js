5bbc59c1fe493d62402a68a2af8bcddf832bd53a
Masahiko Funaki <mfunaki@gmail.com>
2021-03-04 00:10:05 +0900

    Two tests provided.

--- currency/tests/unit/test-handler.js
+++ currency/tests/unit/test-handler.js
@@ -1,22 +1,28 @@
-'use strict';
+"use strict";
 
-const app = require('../../app.js');
-const chai = require('chai');
+const app = require("../../app.js");
+const chai = require("chai");
 const expect = chai.expect;
 var event, context;
 
-describe('Tests index', function () {
-    it('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
+describe("Tests index", function () {
+  it("returns a list of unit/rate when list is specified to command", async () => {
+    var event = { queryStringParameters: { command: "list" } };
+    const result = await app.lambdaHandler(event, context);
 
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
+    expect(result.statusCode).to.equal(200);
+    let response = JSON.parse(result.body);
+    expect(response).to.be.an("array");
+    expect(response[0]).eqls({ unit: "JPY", rate: 1 });
+  });
+  it("converts JPY 2 to USD 208.98", async () => {
+    var event = {
+      queryStringParameters: { command: "convert", unit: "USD", value: 2 },
+    };
+    const result = await app.lambdaHandler(event, context);
 
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
-    });
+    expect(result.statusCode).to.equal(200);
+    let response = JSON.parse(result.body);
+    expect(response).eqls({ amount: 208.98 });
+  });
 });
