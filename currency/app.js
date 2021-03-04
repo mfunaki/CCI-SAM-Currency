d374a84a72fef11c368823d54507c665e9b66378
Masahiko Funaki <mfunaki@gmail.com>
2021-03-04 11:07:35 +0900

    Modified for the initial demo

--- currency/app.js
+++ currency/app.js
@@ -39,7 +39,7 @@ exports.lambdaHandler = async (event, context) => {
                     event.queryStringParameters.value !== undefined
                   ) {
                     value = event.queryStringParameters.value;
-                    amount = Math.round(value * currencies[i].rate * 100) / 100;
+                    amount = value * currencyArray[i].rate;
                     response = {
                       statusCode: 200,
                       body: JSON.stringify({
