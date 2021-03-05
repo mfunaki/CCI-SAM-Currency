bd7d814863c31b9ec18699bd71ee63a6347fe754
Masahiko Funaki <mfunaki@gmail.com>
2021-03-05 18:12:31 +0900

    Error code.

--- currency/app.js
+++ currency/app.js
@@ -40,8 +40,6 @@ exports.lambdaHandler = async (event, context) => {
                   ) {
                     value = event.queryStringParameters.value;
                     amount = value * currencyArray[i].rate;
-                    //amount =
-                    //  Math.round(value * currencyArray[i].rate * 100) / 100;
                     response = {
                       statusCode: 200,
                       body: JSON.stringify({
